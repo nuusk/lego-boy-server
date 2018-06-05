42bb0e1f62b0433e24ce02451af950dd2f48f887
Piotr Ptak <robakzdobywca@gmail.com>
2018-06-05 10:38:06 +0200

    :construction: res send status

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -121,7 +121,7 @@ module.exports = (app) => {
       console.log(project);
       if (project) {
         console.log('juz jest ziom');
-        res.send(409, 'juz jest ziom');
+        res.status(409).send({error: 'Juz jest, ziom.'});
       } else {
         console.log('dodaje nowy');
         db.addProject(newProject)
